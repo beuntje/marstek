b6edc560e777704fd8d97ce5876d31abc91fe859
Benedikt Beun <benedikt.beun@roularta.be>
2026-05-13 16:56:07 +0200

    test

--- test.py
+++ test.py
@@ -9,15 +9,15 @@ homewizard = HomeWizard( Config.get("homewizard.ip") )
 openhab = OpenHAB( Config.get("openhab.url") )
 
 while True:
-    state = openhab.get_item_state("active_power state")
-    print(f"Get:  {state}  ")
+    #state = openhab.get_item_state("active_power")
+    #print(f"Get:  {state}  ")
 
-    #     power = homewizard.get_active_power()
-    #     setPower = 0-power
-    #     setPower = max(-2500, min(2500, setPower))
-    #     if marstek.es_set_mode_passive(power= setPower, cd_time=10):
-    #         print(f"Set power to {setPower} W for 10 seconds")
-    #     else:
-    #         print("✗ Failed to set mode")
+    power = homewizard.get_active_power()
+    setPower = 0-power
+    setPower = max(-2500, min(2500, setPower))
+    if marstek.es_set_mode_passive(power= setPower, cd_time=10):
+        print(f"Set power to {setPower} W for 10 seconds")
+    else:
+        print("✗ Failed to set mode")
 
     time.sleep(10)
