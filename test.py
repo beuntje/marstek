e9c9971972fe8f501b4775f9672ae8b912266e32
Benedikt Beun <benedikt.beun@roularta.be>
2026-05-13 16:45:37 +0200

    test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from utils.config import Config
 from utils.homewizard import HomeWizard
 from utils.marstek import MarstekClient
+import time
 
 marstek = MarstekClient(Config.get("marstek.ip"), Config.get("marstek.port"))
 homewizard = HomeWizard( Config.get("homewizard.ip") )
@@ -9,7 +10,8 @@ homewizard = HomeWizard( Config.get("homewizard.ip") )
 while True:
     power = homewizard.get_active_power()
     setPower = 0-power
-    if client.es_set_mode_passive(power= setPower, cd_time=10):
+    setPower = max(-2500, min(2500, setPower))
+    if marstek.es_set_mode_passive(power= setPower, cd_time=10):
         print(f"Set power to {setPower} W for 10 seconds")
     else:
         print("✗ Failed to set mode")
